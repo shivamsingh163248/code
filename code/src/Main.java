f33990cdc3167c7066aef9e1d5abbce0a57076c3
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-02-21 01:17:44 +0530

    changed

--- code/src/Main.java
+++ code/src/Main.java
@@ -9,7 +9,8 @@ class Main {
         System.out.println("Username is: " + userName);  // Output user input
         System.out.println("also my name is  singh");
         // now we are the trying the written the code again
-        System.out.println("now date is the 16-2-2023 again we writing the again i want to practice again ");
+        System.out.println("now date is the 16-2-2023 again we writing the  ");
+        System.out.println("now the chacking date ");
 
     }
 }
